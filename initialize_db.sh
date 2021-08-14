18c8523d480536b17f2994405e594faf3b35c95a
SuperSonic <supersonic@livemail.tw>
2021-08-14 15:12:21 +0800

    Update

--- initialize_db.sh
+++ initialize_db.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-$TEMP_SQL=/tmp/initialize.sql
+TEMP_SQL=/tmp/initialize.sql
 
-wget -O $TEMP_SQL https://raw.githubusercontent.com/supersonictw/popcat-echo/main/initialize.sql
+## cURL required
+if ! command -v curl &>/dev/null; then
+    apt-get update
+    apt-get install -y curl
+fi
+
+## Start
+curl https://raw.githubusercontent.com/supersonictw/popcat-echo/main/initialize.sql -o $TEMP_SQL
 mysql -u $MARIADB_USER -p $MARIADB_PASS $MARIADB_DATABASE <$TEMP_SQL
 rm $TEMP_SQL
